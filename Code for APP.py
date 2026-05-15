66c084662f18f54ca95000c82b08fe228d05fa3a
Urail-croise <amaury.pika@gmail.com>
2026-05-15 11:03:23 +0200

    Update Code for APP.py

--- Code for APP.py
+++ Code for APP.py
@@ -5,3 +5,8 @@ import parcoursup_programs_medium_2500
 import parcoursup_programs_small_800
 import parcoursup_small_10000
 import pandas as pd
+
+sd = pd.read_csv('parcoursup_medium.csv') 
+dict_student= df.to_dict() #transform the cvs file into a list of dictionary
+
+print(dict_stundent[0])
