608dfe4d60918ba65bb59ebb30eb90d1e06d5ded
AnoukAmillard <anouk.amillard@esme.fr>
2026-05-15 11:25:24 +0200

    Update Code for APP.py

--- Code for APP.py
+++ Code for APP.py
@@ -1,9 +1,10 @@
 import csv
 
-with open ('parcoursup_massive_500000', 'r') as file:
+with open ('parcoursup_massive_500000', 'parcoursup_medium_100000','parcoursup_programs_massive_5000','parcoursup_programs_medium_2500', 'parcoursup_programs_small_800','parcoursup_small_10000' ,'r') as file:
   reader = csv.reader(file, delimiter=';')
   for row in reader:
-    print row
+    print (row)
+
 import parcoursup_massive_500000
 import parcoursup_medium_100000
 import parcoursup_programs_massive_5000
