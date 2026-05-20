ccb6575138561cf59fef1d5012c96e68ad7ce15b
AnoukAmillard <anouk.amillard@esme.fr>
2026-05-20 15:49:41 +0200

    Update Code for APP.py

--- Code for APP.py
+++ Code for APP.py
@@ -5,10 +5,3 @@ with open ('parcoursup_massive_500000', 'parcoursup_medium_100000','parcoursup_p
   for row in reader:
     print (row)
 
-import parcoursup_massive_500000
-import parcoursup_medium_100000
-import parcoursup_programs_massive_5000
-import parcoursup_programs_medium_2500
-import parcoursup_programs_small_800
-import parcoursup_small_10000
-
