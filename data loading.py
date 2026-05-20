94b6e188d5f808bd2192c4b4f66fba5e3c9721d0
AnoukAmillard <anouk.amillard@esme.fr>
2026-05-20 17:50:21 +0200

    Update data loading.py

--- data loading.py
+++ data loading.py
@@ -1,6 +1,18 @@
 import csv
+files = [
+'parcoursup_medium_100000.csv',
+'parcoursup_programs_massive_5000.csv',
+'parcoursup_programs_medium_2500.csv',
+'parcoursup_programs_small_800.csv',
+'parcoursup_small_10000.csv',
+'parcoursup_massive_500000.csv'
+]
 
-with open ('parcoursup_massive_500000', 'parcoursup_medium_100000','parcoursup_programs_massive_5000','parcoursup_programs_medium_2500', 'parcoursup_programs_small_800','parcoursup_small_10000' ,'r') as file:
-  reader = csv.reader(file, delimiter=';')
-  for row in reader:
-    print (row)
+for data in files:
+  print(f‘data:’)
+  with open (data,'r', encoding=“utf-8“) as file:
+    reader = csv.reader(file, delimiter=';')
+    for row in reader:
+      print (row)
+
+
