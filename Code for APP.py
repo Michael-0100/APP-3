2e64ea07e504ce004e65eb344c1bded7f4ee7828
AnoukAmillard <anouk.amillard@esme.fr>
2026-05-21 17:00:29 +0200

    Update Code for APP.py

--- Code for APP.py
+++ Code for APP.py
@@ -1,19 +1,27 @@
 import csv
+
+def load_parcoursup(files):
+  datas=[]
+  
+  for dt in files:
+  print(f'data:')
+  with open (dt,'r', encoding='utf-8') as file:
+    reader = csv.DictReader(file, delimiter=';')
+    for row in reader:
+      data.append(row)
+      
+    return data
+
 files = [
-'parcoursup_medium_100000.csv',
-'parcoursup_programs_massive_5000.csv',
-'parcoursup_programs_medium_2500.csv',
-'parcoursup_programs_small_800.csv',
-'parcoursup_small_10000.csv',
-'parcoursup_massive_500000.csv'
+  'parcoursup_medium_100000.csv',
+  'parcoursup_programs_massive_5000.csv',
+  'parcoursup_programs_medium_2500.csv',
+  'parcoursup_programs_small_800.csv',
+  'parcoursup_small_10000.csv',
+  'parcoursup_massive_500000.csv'
 ]
 
-for data in files:
-  print(f'data:')
-  with open (data,'r', encoding='utf-8') as file:
-    reader = csv.reader(file, delimiter=';')
-    for row in reader:
-      print (row)
+dataset = load_parcoursup(files)
 
-
+print(3
 #testing brand
